c5ab2d310f4842633805f12cff882acdc36374c0
Mincho077 <MinchoNachev@proton.me>
2024-11-30 09:26:20 +0200

    Created Web Infrastructure

---
+++ MountainTrainingApp.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace MountainTrainingApp.Web.Infrastructure.Extensions
+{
+    using System.Security.Claims;
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
+        }
+    }
+}
---
+++ MountainTrainingApp.Web.Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Reflection;
+
+namespace MountainTrainingApp.Web.Infrastructure.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        //public static void AddApplicationServices(this IServiceCollection services, Type serviceType)
+        //{
+        //    Assembly? serciceAssembly = Assembly.GetAssembly(serviceType);
+
+        //    if (serciceAssembly == null)
+        //    {
+        //        throw new InvalidOperationException("Invalid service type provider!");
+        //    }
+
+        //    Type[] serviceTypes = serciceAssembly
+        //        .GetTypes()
+        //        .Where(t => t.Name.EndsWith("Service") && !t.IsInterface)
+        //        .ToArray();
+
+        //    foreach (var implementationType in serviceTypes)
+        //    {
+        //        Type? interfaceType = implementationType.GetInterface($"I{implementationType.Name}");
+        //        if (interfaceType == null)
+        //        {
+        //            throw new InvalidOperationException
+        //                ($"No iterface is provided for service with name {implementationType.Name}");
+        //        }
+
+        //        services.AddScoped(interfaceType, implementationType);
+        //    }
+
+        //}
+    }
+}
