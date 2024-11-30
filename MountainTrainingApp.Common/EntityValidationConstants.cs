f06e68211ee0b5bffd36094b7eeb1d312e2d1c75
Mincho077 <MinchoNachev@proton.me>
2024-11-30 08:48:31 +0200

    Added Commnon static classes

---
+++ MountainTrainingApp.Common/EntityValidationConstants.cs
@@ -0,0 +1,8 @@
+﻿
+namespace MountainTrainingApp.Common
+{
+    public static class EntityValidationConstants
+    {
+
+    }
+}
---
+++ MountainTrainingApp.Common/NotificationMessagesConstants.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MountainTrainingApp.Common
+{
+    public static class NotificationMessagesConstants
+    {
+        public const string ErrorMesage = "ErrorMesage";
+        public const string WarningMessage = "WarningMessage";
+        public const string InformationMessage = "InfoMessage";
+        public const string SuccesMessage = "SuccesMessage";
+    }
+}
