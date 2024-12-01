199fd0b8a07631193d6675d6973e6c0693ebb387
Mincho077 <MinchoNachev@proton.me>
2024-12-01 11:47:03 +0200

    Created AerobicActivityConfiguration

---
+++ MountainTrainingApp.Data/Configurations/AerobicActivityConfiguration.cs
@@ -0,0 +1,74 @@
+﻿namespace MountainTrainingApp.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using MountainTrainingApp.Data.Models;
+    public class AerobicActivityConfiguration : IEntityTypeConfiguration<AerobicActivity>
+    {
+        public void Configure(EntityTypeBuilder<AerobicActivity> builder)
+        {
+            builder.HasData(GenerateAerobicActivities());
+        }
+        private AerobicActivity[] GenerateAerobicActivities()
+        {
+            ICollection< AerobicActivity >aerobicActivities=new HashSet< AerobicActivity >();
+
+            AerobicActivity aerobicActivity;
+
+            aerobicActivity = new AerobicActivity()
+            {
+                Id = 1,
+                Name = "Trail running",
+            };
+
+            aerobicActivities.Add(aerobicActivity);
+
+            aerobicActivity = new AerobicActivity()
+            {
+                Id = 2,
+                Name = "Hiking",
+            };
+
+            aerobicActivity = new AerobicActivity()
+            {
+                Id = 3,
+                Name = "Mountain biking",
+            };
+            aerobicActivities.Add(aerobicActivity);
+
+            aerobicActivity = new AerobicActivity()
+            {
+                Id = 4,
+                Name = "Road biking",
+            };
+
+            aerobicActivities.Add(aerobicActivity);
+
+            aerobicActivity = new AerobicActivity()
+            {
+                Id = 5,
+                Name = "Road running",
+            };
+
+            aerobicActivities.Add(aerobicActivity);
+
+            aerobicActivity = new AerobicActivity()
+            {
+                Id = 6,
+                Name = "Cross-country skiing",
+            };
+
+            aerobicActivities.Add(aerobicActivity);
+
+            aerobicActivity = new AerobicActivity()
+            {
+                Id = 7,
+                Name = "Swimming",
+            };
+
+            aerobicActivities.Add(aerobicActivity);
+
+            return aerobicActivities.ToArray();
+        }
+    }
+}
