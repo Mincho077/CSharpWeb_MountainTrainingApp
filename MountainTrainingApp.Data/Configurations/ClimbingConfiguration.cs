74cc6058d10dce0d9df907bb2b24ba0a60f80e17
Mincho077 <MinchoNachev@proton.me>
2024-12-01 16:07:55 +0200

    Created ClimbingConfiguration

---
+++ MountainTrainingApp.Data/Configurations/ClimbingConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace MountainTrainingApp.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using MountainTrainingApp.Data.Models;
+    public class ClimbingConfiguration : IEntityTypeConfiguration<Climbing>
+    {
+        public void Configure(EntityTypeBuilder<Climbing> builder)
+        {
+            builder
+               .HasOne(m => m.ClimbingActivity)
+               .WithMany(mt => mt.Climbings)
+               .HasForeignKey(m => m.ClimbingActivityId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+             .HasOne(m => m.TrainingPeriod)
+             .WithMany(mt => mt.Climbings)
+             .HasForeignKey(m => m.TrainingPeriodId)
+             .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
