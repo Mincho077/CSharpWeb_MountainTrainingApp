477e1e8ae5d33942481520de82fd9e2f465f5352
Mincho077 <MinchoNachev@proton.me>
2024-12-01 16:33:47 +0200

    Modified ApplicationUser

--- MountainTrainingApp.Data.Models/ApplicationUser.cs
+++ MountainTrainingApp.Data.Models/ApplicationUser.cs
@@ -7,9 +7,13 @@
         {
             AerobicWorkouts=new HashSet<AerobicWorkout>();
             StrengthWorkouts=new HashSet<StrengthWorkout>();
+            Climbings=new HashSet<Climbing>();
         }
         public virtual IEnumerable<AerobicWorkout> AerobicWorkouts { get; set; }
 
         public virtual IEnumerable<StrengthWorkout> StrengthWorkouts { get; set; }
+
+        public virtual IEnumerable<Climbing> Climbings { get; set; }
+
     }
 }
