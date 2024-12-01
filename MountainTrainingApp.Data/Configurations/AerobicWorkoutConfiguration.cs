38e21b15899543b03b020429a73bda606c43ddb7
Mincho077 <MinchoNachev@proton.me>
2024-12-01 16:00:32 +0200

    Created AerobicWorkoutConfiguration

---
+++ MountainTrainingApp.Data/Configurations/AerobicWorkoutConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace MountainTrainingApp.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using MountainTrainingApp.Data.Models;
+    public class AerobicWorkoutConfiguration : IEntityTypeConfiguration<AerobicWorkout>
+    {    
+        public void Configure(EntityTypeBuilder<AerobicWorkout> builder)
+        {
+            builder
+                .HasOne(m => m.AerobicActivity)
+                .WithMany(mt => mt.AerobicWorkouts)
+                .HasForeignKey(m => m.AerobicActivityId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(m => m.TrainingPeriod)
+                .WithMany(mf => mf.AerobicWorkouts)
+                .HasForeignKey(m => m.TrainingPeriodId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+    }
+}
