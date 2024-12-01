85a15c65956b5a3b6a9b211f4f62affffa4fa29a
Mincho077 <MinchoNachev@proton.me>
2024-12-01 11:20:48 +0200

    Created  Climbing and ClimbingActivity classes

---
+++ MountainTrainingApp.Data.Models/Climbing.cs
@@ -0,0 +1,52 @@
+﻿namespace MountainTrainingApp.Data.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    public class Climbing
+    {
+        public Climbing()
+        {
+            Id =Guid.NewGuid();
+        }
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public DayOfWeek DayOfWeek { get; set; }
+
+        [Required]
+        public DateTime DateAndTime { get; set; }
+
+        [Required]
+        public double Duration { get; set; }
+
+        [Required]
+        public int RopesClimbed { get; set; }
+
+        [Required]
+        public int ClimbingActivityId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(ClimbingActivityId))]
+        public virtual ClimbingActivity ClimbingActivity { get; set; } = null!;
+
+        [Required]
+        public int TrainingPeriodId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(TrainingPeriodId))]
+        public virtual TrainingPeriod TrainingPeriod { get; set; } = null!;
+
+        [Required]
+        public Guid AthletId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(AthletId))]
+        public virtual ApplicationUser Athlet { get; set; } = null!;
+
+        public Guid? TrainerId { get; set; }
+
+        [ForeignKey(nameof(TrainerId))]
+        public virtual Trainer? Trainer { get; set; }
+    }
+}
---
+++ MountainTrainingApp.Data.Models/ClimbingActivity.cs
@@ -0,0 +1,15 @@
+﻿namespace MountainTrainingApp.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Common.EntityValidationConstants.ClimbingActivityConstants;
+    public class ClimbingActivity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+    }
+}
