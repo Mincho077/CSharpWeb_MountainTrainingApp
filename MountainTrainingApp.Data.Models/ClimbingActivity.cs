0db5d9923a0eab8c7a10780b332a3dcc92f4b4d9
Mincho077 <MinchoNachev@proton.me>
2024-12-01 11:24:10 +0200

    Added  Climbings collection to ClimbingActivity

--- MountainTrainingApp.Data.Models/ClimbingActivity.cs
+++ MountainTrainingApp.Data.Models/ClimbingActivity.cs
@@ -4,12 +4,18 @@
     using static Common.EntityValidationConstants.ClimbingActivityConstants;
     public class ClimbingActivity
     {
+        public ClimbingActivity()
+        {
+            Climbings=new HashSet<Climbing>();
+        }
         [Key]
         public int Id { get; set; }
 
         [Required]
         [MaxLength(NameMaxLength)]
         public string Name { get; set; } = null!;
+
+        public virtual IEnumerable<Climbing> Climbings { get; set; }
 
     }
 }
