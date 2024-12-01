dde7fe464dd7258eb1b50fef2691b0360d57bd4c
Mincho077 <MinchoNachev@proton.me>
2024-12-01 16:30:14 +0200

    Added StrengthWokoutConfiguration class

---
+++ MountainTrainingApp.Data/Configurations/StrengthWorkoutConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace MountainTrainingApp.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using MountainTrainingApp.Data.Models;
+    public class StrengthWorkoutConfiguration : IEntityTypeConfiguration<StrengthWorkout>
+    {
+        public void Configure(EntityTypeBuilder<StrengthWorkout> builder)
+        {
+            builder
+              .HasMany(m => m.StrengthExercises)
+              .WithMany(mt => mt.StrengthWorkouts);
+              
+            builder
+             .HasOne(m => m.TrainingPeriod)
+             .WithMany(mt => mt.StrengthWorkouts)
+             .HasForeignKey(m => m.TrainingPeriodId)
+             .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
