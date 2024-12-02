8d89d4ceddec91935c5355ea6f9c5c0b405c5c5e
Mincho077 <MinchoNachev@proton.me>
2024-12-02 08:44:36 +0200

    Added StrengthWorkoutType class

---
+++ MountainTrainingApp.Data.Models/StrengthWorkoutType.cs
@@ -0,0 +1,25 @@
+﻿namespace MountainTrainingApp.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Common.EntityValidationConstants.StrengthWorkoutTypeConstants;
+    public class StrengthWorkoutType
+    {
+        public StrengthWorkoutType()
+        {
+            StrengthExercises = new HashSet<StrengthExercise>();
+            StrengthWorkouts = new HashSet<StrengthWorkout>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        public virtual IEnumerable<StrengthWorkout> StrengthWorkouts { get; set; }
+
+        public virtual IEnumerable<StrengthExercise> StrengthExercises { get; set; }
+
+    }
+}
