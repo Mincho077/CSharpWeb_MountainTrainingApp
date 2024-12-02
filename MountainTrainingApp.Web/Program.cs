af7cb1067ab10334f243344fac523a9f0d5272a2
Mincho077 <MinchoNachev@proton.me>
2024-12-02 11:30:57 +0200

    Modified Program and apsettings

--- MountainTrainingApp.Web/Program.cs
+++ MountainTrainingApp.Web/Program.cs
@@ -18,8 +18,17 @@ namespace MountainTrainingApp.Web
                 options.UseSqlServer(connectionString));
 
             builder.Services.AddDefaultIdentity<ApplicationUser>(options => 
-            { 
-                options.SignIn.RequireConfirmedAccount = false; 
+            {
+                options.SignIn.RequireConfirmedAccount =
+                  builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedAccount");
+                options.Password.RequireLowercase =
+                   builder.Configuration.GetValue<bool>("Identity:Password:RequireLowercase");
+                options.Password.RequireUppercase =
+                   builder.Configuration.GetValue<bool>("Identity:Password:RequireUppercase");
+                options.Password.RequireNonAlphanumeric =
+                   builder.Configuration.GetValue<bool>("Identity:Password:RequireNonAlphanumeric");
+                options.Password.RequiredLength =
+                   builder.Configuration.GetValue<int>("Identity:Password:RequiredLength");
             })
                 .AddEntityFrameworkStores<MountainTrainigAppDbContext>();
             builder.Services.AddControllersWithViews();
