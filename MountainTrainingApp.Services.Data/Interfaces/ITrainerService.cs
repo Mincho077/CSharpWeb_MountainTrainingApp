da00e63eae6376f90c85aca143ba35d71d71be7e
Mincho077 <MinchoNachev@proton.me>
2024-12-03 11:02:10 +0200

    Added ITrainerService

---
+++ MountainTrainingApp.Services.Data/Interfaces/ITrainerService.cs
@@ -0,0 +1,9 @@
+﻿namespace MountainTrainingApp.Services.Data.Interfaces
+{
+    public interface ITrainerService
+    {
+        Task<bool> TainerExistByUserIdAsync(string userId);
+
+        Task<bool> TarnerExistNameIdAsync(string phoneNumber);
+    }
+}
