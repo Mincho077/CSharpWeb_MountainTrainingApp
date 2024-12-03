8fdc180875ecae496af75e3f11e8410b7d5ab881
Mincho077 <MinchoNachev@proton.me>
2024-12-03 10:44:12 +0200

    #

---
+++ MountainTrainingApp.Web/Controllers/AerobicWorkoutController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MountainTrainingApp.Web.Controllers
+{
+    public class AerobicWorkoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Mine()
+        {
+            return View();
+        }
+    }
+}
---
+++ MountainTrainingApp.Web/Controllers/TrainerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MountainTrainingApp.Web.Controllers
+{
+    public class TrainerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
