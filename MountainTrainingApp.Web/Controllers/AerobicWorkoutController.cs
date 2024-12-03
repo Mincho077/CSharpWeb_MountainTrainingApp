8421e8c37d33b6d3fcf96637e24b9c855a88206f
Mincho077 <MinchoNachev@proton.me>
2024-12-03 11:10:54 +0200

    Modified AerobicWorkoutController

--- MountainTrainingApp.Web/Controllers/AerobicWorkoutController.cs
+++ MountainTrainingApp.Web/Controllers/AerobicWorkoutController.cs
@@ -1,6 +1,9 @@
 ﻿namespace MountainTrainingApp.Web.Controllers
 {
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
     public class AerobicWorkoutController : Controller
     {
         public async Task<IActionResult> Index()
