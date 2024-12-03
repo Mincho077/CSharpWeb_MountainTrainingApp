73c38b5608c69e95a9088ba0e8adc4cdddfbdc41
Mincho077 <MinchoNachev@proton.me>
2024-12-03 20:01:31 +0200

    Modified ITrainerService

--- MountainTrainingApp.Services.Data/Interfaces/ITrainerService.cs
+++ MountainTrainingApp.Services.Data/Interfaces/ITrainerService.cs
@@ -5,6 +5,7 @@ namespace MountainTrainingApp.Services.Data.Interfaces
     public interface ITrainerService
     {
         Task CreateTrainerAsync(string userId, RegisterTrainerViewModel model);
+
         Task<bool> TainerExistByUserIdAsync(string userId);
 
         Task<bool> TarnerExistNameIdAsync(string phoneNumber);
