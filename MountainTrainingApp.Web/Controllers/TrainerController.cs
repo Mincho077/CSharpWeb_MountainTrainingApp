ed1c966612f596a5740c7c557aa714775ab09fdf
Mincho077 <MinchoNachev@proton.me>
2024-12-03 19:50:32 +0200

    Modified Trainer Controller

--- MountainTrainingApp.Web/Controllers/TrainerController.cs
+++ MountainTrainingApp.Web/Controllers/TrainerController.cs
@@ -2,12 +2,79 @@
 {
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using  Services.Data.Interfaces;
+    using static Infrastructure.Extensions.ClaimsPrincipalExtensions;
+    using static Common.NotificationMessagesConstants;
+    using Web.ViewModels.Trainer;
     [Authorize]
     public class TrainerController : Controller
     {
-        public async Task<IActionResult> Index()
+        private readonly ITrainerService trainerService;
+        public TrainerController(ITrainerService trainerService)
         {
+            this.trainerService = trainerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RegisterTrainer()
+        {
+            string userId = User.GetUserId();
+
+            bool isTrainer=await trainerService
+                .TainerExistByUserIdAsync(userId);
+
+            if (isTrainer)
+            {
+                TempData[ErrorMesage] = "Нали се регистрира ве,кретен!";
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterTrainer(RegisterTrainerViewModel model)
+        {
+            string userId = User.GetUserId();
+
+            bool isTrainer = await trainerService
+                .TainerExistByUserIdAsync(userId);
+
+            if (isTrainer)
+            {
+                TempData[ErrorMesage] = "Нали се регистрира ве,кретен!";
+                return RedirectToAction("Index", "Home");
+            }
+
+            bool isNameTaken=await trainerService
+                .TarnerExistNameIdAsync(model.Name);
+            if (isNameTaken) 
+            {
+                ModelState.AddModelError(nameof(model.Name), "Мноо си прост ве пич,има вече такова име!");
+                return RedirectToAction("Index", "Home");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                await trainerService.CreateTrainerAsync(userId,model);
+            }
+            catch (Exception)
+            {
+
+                TempData[ErrorMesage] = "Unexpected error occurred while register you as a Trainer." +
+                    "Please try again later or contact administrator";
+
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
