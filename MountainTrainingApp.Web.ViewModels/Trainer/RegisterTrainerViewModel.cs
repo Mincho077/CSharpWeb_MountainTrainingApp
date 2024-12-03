6d48974fe559da734f23d161e21c4e123bebc465
Mincho077 <MinchoNachev@proton.me>
2024-12-03 19:09:07 +0200

    Added RegisterTrainerViewModel

---
+++ MountainTrainingApp.Web.ViewModels/Trainer/RegisterTrainerViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MountainTrainingApp.Web.ViewModels.Trainer
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Common.EntityValidationConstants.TrainerConstats;
+    public class RegisterTrainerViewModel
+    {
+        [Required]
+        [MinLength(NameMinLength)]
+        [MaxLength(NameMaxLength)]
+        [Display(Name = "Trainer name")]
+        public string Name { get; set; } = null!;
+    }
+}
