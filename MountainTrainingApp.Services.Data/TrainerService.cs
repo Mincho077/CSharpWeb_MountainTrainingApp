5c9af6a778b07ddbab6ce719308522a7961e126f
Mincho077 <MinchoNachev@proton.me>
2024-12-03 11:53:31 +0200

    Modified TrainerService

--- MountainTrainingApp.Services.Data/TrainerService.cs
+++ MountainTrainingApp.Services.Data/TrainerService.cs
@@ -1,7 +1,17 @@
-﻿namespace MountainTrainingApp.Services.Data
-{
-    public class TrainerService
-    {
+﻿using MountainTrainingApp.Services.Data.Interfaces;
 
+namespace MountainTrainingApp.Services.Data
+{
+    public class TrainerService : ITrainerService
+    {
+        public Task<bool> TainerExistByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> TarnerExistNameIdAsync(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
