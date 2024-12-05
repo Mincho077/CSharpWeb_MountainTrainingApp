a1a9fda82f91cc781ed560eca15b71d040293911
Mincho077 <MinchoNachev@proton.me>
2024-12-05 19:52:10 +0200

    Added Aerobic Activity Service

---
+++ MountainTrainingApp.Services.Data/AerobicActivityService.cs
@@ -0,0 +1,34 @@
+﻿namespace MountainTrainingApp.Services.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MountainTrainingApp.Data;
+    using MountainTrainingApp.Services.Data.Interfaces;
+    using MountainTrainingApp.Web.ViewModels.AerobicActivity;
+
+    public class AerobicActivityService : IAerobicActivityService
+    {
+        private readonly MountainTrainigAppDbContext context;
+        public AerobicActivityService(MountainTrainigAppDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<bool> AerobicActivityExistByIdAsync(int id)
+        {
+            return await context.AerobicActivities
+            .AnyAsync(tp => tp.Id == id);
+        }
+
+        public async Task<IEnumerable<AerobicActivityViewMode>> AllAerobicActivitiesAsync()
+        {
+           
+            return await context.AerobicActivities
+            .AsNoTracking()
+            .Select(tp => new AerobicActivityViewMode()
+            {
+                Id = tp.Id,
+                Name = tp.Name,
+            })
+            .ToArrayAsync();
+        }
+    }
+}
