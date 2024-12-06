2a1923608f75c9522dee961fc786c4fb184704af
Mincho077 <MinchoNachev@proton.me>
2024-12-06 11:23:10 +0200

    Modified Aerobic Workout Controller

--- MountainTrainingApp.Web/Controllers/AerobicWorkoutController.cs
+++ MountainTrainingApp.Web/Controllers/AerobicWorkoutController.cs
@@ -2,10 +2,26 @@
 {
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using Web.ViewModels.AerobicWorkout;
+    using Services.Data.Interfaces;
 
     [Authorize]
     public class AerobicWorkoutController : Controller
     {
+        private readonly IAerobicActivityService aerobicActivityService;
+        private readonly IAerobicWorkoutService aerobicWorkoutService;
+        private readonly ITrainingPeriodService trainingPeriodService;
+        private readonly IDayOfWeekService dayOfWeekService;
+        public AerobicWorkoutController(IAerobicActivityService aerobicActivityService
+            ,IAerobicWorkoutService aerobicWorkoutService
+            ,ITrainingPeriodService trainingPeriodService
+            ,IDayOfWeekService dayOfWeekService)
+        {
+            this.aerobicActivityService = aerobicActivityService;
+            this.aerobicWorkoutService = aerobicWorkoutService;
+            this.trainingPeriodService = trainingPeriodService;
+            this.dayOfWeekService = dayOfWeekService;
+        }
         public async Task<IActionResult> Index()
         {
             return View();
@@ -13,7 +29,17 @@
 
         public async Task<IActionResult> Add()
         {
-            return View();
+            var model=new AerobicWorkoutAddViewModel();
+            model.AerobicActivities =
+                await aerobicActivityService.AllAerobicActivitiesAsync();
+
+            model.TrainingPeriods=
+                await trainingPeriodService.GetTrainingPeriodsAsync();
+
+            model.DaysOfWeek=
+                await dayOfWeekService.DaysOfWeekAsync();
+
+            return View(model);
         }
 
         public IActionResult Mine()
