3c8d8c8ec8fce482ea21a4523752c860fcee4138
Mincho077 <MinchoNachev@proton.me>
2024-12-06 10:26:11 +0200

    Modified Aerobic Workout Service and IAerobicWorkout interface

--- MountainTrainingApp.Services.Data/AerobicWorkoutService.cs
+++ MountainTrainingApp.Services.Data/AerobicWorkoutService.cs
@@ -1,9 +1,27 @@
-﻿using MountainTrainingApp.Services.Data.Interfaces;
-
-namespace MountainTrainingApp.Services.Data
+﻿namespace MountainTrainingApp.Services.Data
 {
-    public class AerobicWorkoutService:IAerobicWorkoutService
-    {
+    using MountainTrainingApp.Data;
+    using MountainTrainingApp.Data.Models;
+    using MountainTrainingApp.Services.Data.Interfaces;
+    using MountainTrainingApp.Web.ViewModels.AerobicWorkout;
+    using System.Threading.Tasks;
 
+    public class AerobicWorkoutService : IAerobicWorkoutService
+    {
+        private readonly MountainTrainigAppDbContext context;
+        public AerobicWorkoutService(MountainTrainigAppDbContext context)
+        {
+            this.context = context;
+        }
+        public Task CreateAerobicWorkoutAsync(AerobicWorkoutAddViewModel model, string trainerId)
+        {
+            AerobicWorkout workout= new AerobicWorkout()
+            { 
+                Duration=model.Duration,
+                ElevationGain=model.ElevationGain,
+                Distance=model.Distance,
+                AverageHeartRate=model.AverageHeartRate,
+            };
+        }
     }
 }
--- MountainTrainingApp.Services.Data/Interfaces/IAerobicWorkoutService.cs
+++ MountainTrainingApp.Services.Data/Interfaces/IAerobicWorkoutService.cs
@@ -1,7 +1,11 @@
-﻿namespace MountainTrainingApp.Services.Data.Interfaces
+﻿using MountainTrainingApp.Web.ViewModels.AerobicWorkout;
+
+namespace MountainTrainingApp.Services.Data.Interfaces
 {
     public interface IAerobicWorkoutService
     {
-       
+        //Task<IEnumerable<IndexViewModel>> AllMedicinesAsync();
+
+        Task CreateAerobicWorkoutAsync(AerobicWorkoutAddViewModel model, string trainerId);
     }
 }
