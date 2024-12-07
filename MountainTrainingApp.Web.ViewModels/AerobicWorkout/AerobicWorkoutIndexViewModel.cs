a45884878f34a6615846f160837b1fb11191a742
Mincho077 <MinchoNachev@proton.me>
2024-12-07 18:14:36 +0200

    Added AWIndexViewModel

---
+++ MountainTrainingApp.Web.ViewModels/AerobicWorkout/AerobicWorkoutIndexViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MountainTrainingApp.Web.ViewModels.AerobicWorkout
+{
+    public class AerobicWorkoutIndexViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public  string DayOfWeek { get; set; } = null!;
+
+        public string DateAndTime { get; set; } = null!;
+
+        public string Duration { get; set; } = null!;
+
+        public string Distance { get; set; } = null!;
+
+        public string BurnedCalories { get; set; } = null!;
+
+        public string AddedWeight { get; set; } = null!;
+
+        public string ElevationGain { get; set; } = null!;
+
+        public string AverageHeartRate { get; set; } = null!;
+
+        public string AerobicActivity { get; set; } = null!;
+
+        public string TrainingPeriod { get; set; } = null!;
+
+
+    }
+}
