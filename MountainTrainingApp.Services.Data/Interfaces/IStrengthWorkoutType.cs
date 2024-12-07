7a422467b6f96f2b12e095c20bdd00754465d8c1
Mincho077 <MinchoNachev@proton.me>
2024-12-07 19:54:28 +0200

    Added  IStrengthWorkoutType Interface

---
+++ MountainTrainingApp.Services.Data/Interfaces/IStrengthWorkoutType.cs
@@ -0,0 +1,9 @@
+﻿namespace MountainTrainingApp.Services.Data.Interfaces
+{
+    using MountainTrainingApp.Web.ViewModels.StrengthWorkoutType;
+    public interface IStrengthWorkoutType
+    {
+        Task<IEnumerable<StrengthWorkoutTypeViewModel>> AllStrengthWorkoutsAsync();
+        Task<bool> StrengthWorkoutExistByIdAsync(int id);
+    }
+}
---
+++ MountainTrainingApp.Web.ViewModels/StrengthWorkoutType/StrengthWorkoutTypeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MountainTrainingApp.Web.ViewModels.StrengthWorkoutType
+{
+    public class StrengthWorkoutTypeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
